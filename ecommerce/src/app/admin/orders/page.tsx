526552be4ef014abc6aee1515598ae01acf6fcd5
Yaksh <yakshparikh2004@gmail.com>
2025-05-01 15:50:31 +0530

    orders page modification

--- ecommerce/src/app/admin/orders/page.tsx
+++ ecommerce/src/app/admin/orders/page.tsx
@@ -1,3 +1,4 @@
+
 "use client";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
@@ -18,17 +19,12 @@ interface Order {
 
 const AllOrdersPage = () => {
   const [orders, setOrders] = useState<Order[]>([]);
-  //const [loading, setLoading] = useState<boolean>(false);
-  const [orderId, setOrderId] = useState<number | null>(null);
-  const [orderStatus, setOrderStatus] = useState<string>("");
   const [search, setSearch] = useState<string>("");
-  
-  console.log(orderStatus);
+  const [editingStatus, setEditingStatus] = useState<Order | null>(null);
 
-  
+
   const fetchOrders = async () => {
     try {
-      //setLoading(true);
       const response = await axios.get("http://localhost:8000/api/orders", {
         headers: {
           Authorization: `Bearer ${localStorage.getItem("token")}`,
@@ -37,7 +33,7 @@ const AllOrdersPage = () => {
       setOrders(response.data);
     } catch (error) {
       console.error("Error fetching orders:", error);
-    } 
+    }
   };
 
   const deleteOrder = async (id: number) => {
@@ -53,101 +49,127 @@ const AllOrdersPage = () => {
     }
   };
 
-  useEffect(() => {
-    fetchOrders();
-  }, []);
+  // const editOrderStatus = (orderId: number, newStatus: string) => {
+  //   setEditingStatus((prev) => ({
+  //     ...prev,
+  //     [orderId]: newStatus,
+  //   }));
+  // };
 
-  const editOrder = (id: number, status: string) => {
-    setOrderId(id);
-    setOrderStatus(status);
-  };
-  const updateStatus = async () => {
-    const orderState = {
-      status: orderStatus,
+  const updateStatus = async (orderId: number) => {
+    const status = {
+      status: editingStatus?.status || "PENDING",
     }
     try {
       const response = await axios.put(
-        `http://localhost:8000/api/orders/${orderId}/status`,orderState,{
+        `http://localhost:8000/api/orders/${orderId}/status`,
+         status ,
+        {
           headers: {
             Authorization: `Bearer ${localStorage.getItem("token")}`,
           },
         }
       );
-      console.log(response.status);
 
       if (response.status === 200) {
-        alert("Status updated successfully for order ID: " + orderId);
+        alert(`Order ${orderId} status updated successfully.`);
         fetchOrders();
+        // setEditingStatus((prev) => {
+        //   const updated = { ...prev };
+        //   delete updated[orderId];
+        //   return updated;
+        // });
       }
     } catch (err) {
       console.error("Error updating order status:", err);
     }
+    finally{
+      setEditingStatus(null);
+    }
   };
+
+  useEffect(() => {
+    fetchOrders();
+  }, []);
 
   return (
     <div className="p-4">
       <h1 className="text-xl font-bold mb-4">All Orders</h1>
+
       <input
         type="text"
-        placeholder="Search by Status"
+        placeholder="Search by Status or Order ID"
         value={search}
         onChange={(e) => setSearch(e.target.value)}
         className="mb-4 p-2 border rounded m-2"
       />
 
-      <select
-        name=""
-        id=""
-        value={orderStatus}
-        onChange={(e) => setOrderStatus(e.target.value)}
-        className="mb-4 p-2 border rounded"
-      >
-        <option value="">None</option>
-        <option value="PENDING"> PENDING</option>
-        <option value="PROCESSING">PROCESSING</option>
-        <option value="SHIPPED">SHIPPED</option>
-        <option value="DELIVERED">DELIVERED</option>
-        <option value="CANCELLED">CANCELLED</option>
-      </select>
-      <button 
-        onClick={() => updateStatus()}
-        className="bg-blue-500 text-white px-4 py-2 rounded m-2"
-      >
-        Add
-      </button>
-      {false ? (
-        <p>Loading orders...</p>
-      ) : (
-        <table className="min-w-full border border-gray-200 text-sm">
-          <thead className="bg-gray-100">
-            <tr>
-              <th className="border px-3 py-2">ORDER_ID</th>
-              <th className="border px-3 py-2">Buyer</th>
-              <th className="border px-3 py-2">Product_ID</th>
-              <th className="border px-3 py-2">Product_Name</th>
-              <th className="border px-3 py-2">Status</th>
-              <th className="border px-3 py-2">Action</th>
-            </tr>
-          </thead>
-          <tbody>
-            {orders.filter((value)=>search.toLowerCase()===""? value: value.status.startsWith(search)).map((order) =>
-              order.products.map((product) => (
-                <tr key={order.order_id} className="text-center">
+      <table className="min-w-full border border-gray-200 text-sm">
+        <thead className="bg-gray-100">
+          <tr>
+            <th className="border px-3 py-2">ORDER_ID</th>
+            <th className="border px-3 py-2">Buyer</th>
+            <th className="border px-3 py-2">Product_ID</th>
+            <th className="border px-3 py-2">Product_Name</th>
+            <th className="border px-3 py-2">Status</th>
+            <th className="border px-3 py-2">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {orders
+            .filter((order) =>
+              search === "" ? true : (
+                order.status.toLowerCase().startsWith(search.toLowerCase()) || 
+                order.order_id.toString().startsWith(search.toLowerCase())
+              )
+            )
+            .map((order) =>
+              order.products.map((product, index) => (
+                <tr key={`${order.order_id}-${index}`} className="text-center">
                   <td className="border px-3 py-1">{order.order_id}</td>
                   <td className="border px-3 py-1">{order.buyer.name}</td>
                   <td className="border px-3 py-1">{product.product_id}</td>
                   <td className="border px-3 py-1">{product.name}</td>
-                  <td className="border px-3 py-1">{order.status}</td>
-                  <td className="border px-3 py-1 space-x-2">
-                    <button 
-                      onClick={() => editOrder(order.order_id, order.status)}
-                      className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-red-600 "
+                  {editingStatus?.order_id===order.order_id ? <td className="border px-3 py-1">
+                   <select
+                      value={editingStatus.status}
+                      onChange={(e) => setEditingStatus({...editingStatus, status: e.target.value})}
+                      className="border p-1 rounded"
                     >
-                      Edit
+                      <option value="PENDING">PENDING</option>
+                      <option value="PROCESSING">PROCESSING</option>
+                      <option value="SHIPPED">SHIPPED</option>
+                      <option value="DELIVERED">DELIVERED</option>
+                      <option value="CANCELLED">CANCELLED</option>
+                    </select>
+                  </td>
+                  :
+                  <td className="border px-3 py-1">{order.status}</td>
+                  }
+                  <td className="border px-3 py-1 space-x-2">
+                 {editingStatus?.order_id===order.order_id ? 
+                    <button
+                      onClick={() => updateStatus(order.order_id)}
+                      className="bg-green-500 text-white px-2 py-1 rounded hover:bg-green-600"
+                    >
+                      Update
                     </button>
-                    <button 
+                  
+                   
+                    :
+                   
+                    <button
+                      onClick={() => setEditingStatus(order)}
+                      className="bg-blue-500 text-white px-2 py-1 rounded hover:bg-green-600"
+                    >
+                    Edit
+                    </button>
+                    
+                  }
+                    
+                    <button
                       onClick={() => deleteOrder(order.order_id)}
-                      className=" bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
+                      className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-600"
                     >
                       Delete
                     </button>
@@ -155,22 +177,18 @@ const AllOrdersPage = () => {
                 </tr>
               ))
             )}
-            {orders.length === 0 && (
-              <tr>
-                <td colSpan={6} className="text-center py-4">
-                  No orders found.
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      )}
+          {orders.length === 0 && (
+            <tr>
+              <td colSpan={6} className="text-center py-4">
+                No orders found.
+              </td>
+            </tr>
+          )}
+        </tbody>
+      </table>
     </div>
   );
 };
 
 export default AllOrdersPage;
-
-
-
 
--- ecommerce/src/app/admin/users/page.tsx
+++ ecommerce/src/app/admin/users/page.tsx
@@ -1,74 +1,176 @@
+// "use client";
+
+// import axios from "axios";
+// import { useEffect, useState } from "react";
+
+// export default function UsersPage() {
+//   const [users, setUsers] = useState<
+//     { id: number; name: string; email: string; role: string }[]
+//   >([]);
+//   const [search, setSearch] = useState<string>("");
+//   console.log(users);
+//   console.log(users);
+  
+
+//   useEffect(() => {
+//     const fetchUsers = async () => {
+//       try {
+//         const res = await axios.get(
+//           "http://localhost:8000/api/admin/users",
+
+//           {
+//             headers: {
+//               Authorization: `Bearer ${localStorage.getItem("token")}`,
+//             },
+//           }
+//         );
+      
+
+//         if (res.status === 200) {
+      
+//           setUsers(res.data);
+//         } else {
+//           alert("Failed to load Users");
+//         }
+//       } catch (err) {
+//         console.error(err);
+//         alert("Error fetching Users");
+//       }
+//     };
+//     fetchUsers();
+//   }, []);
+
+//   const handleDelete = async (id: number) => {
+//     try {
+//       const res = await axios.delete(
+//         `http://localhost:8000/api/admin/users/${id}`,
+//         {
+//           headers: {
+//             Authorization: `Bearer ${localStorage.getItem("token")}`,
+//           },
+//         }
+//       );
+//       console.log(res);
+      
+//       if (res.status === 200) {
+//         setUsers(users.filter((user) => Number(user.id) !== id));
+//         alert("User deleted successfully");
+//       } else {
+//         alert("Failed to delete User");
+//       }
+//     } catch (err) {
+//       console.error(err);
+//       alert("Error deleting User");
+//     }
+//   }
+  
+
+
+//   return (
+//     <div className="p-4">
+//       <h1 className="text-xl font-bold mb-4">All Users</h1>
+//       <input
+//         type="text"
+//         placeholder="Search by Name"
+//         value={search}
+//         onChange={(e) => setSearch(e.target.value)}
+//         className="border p-2 mb-4 rounded w-full"
+//       />
+//       <ul className="space-y-2">
+//         {users.filter((value)=>search.toLowerCase()===""? value: value.name.startsWith(search)).map((user) => (
+//           <li key={user.id} className="border p-3 rounded-md">
+//             <p>
+//               <b>Name:</b> {user.name}
+//             </p>
+//             <p>
+//               <b>Email:</b> {user.email}
+//             </p>
+//             <p>
+//               <b>Role:</b> {user.role}
+//             </p>
+//             <button className="bg-blue-500 text-white px-3 py-1 rounded" onClick={() => handleDelete(Number(user.id))}>
+//               Delete
+//             </button>
+//           </li>
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
 "use client";
+import axios from 'axios';
+import React, { useEffect, useState } from 'react'
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: string;
+}
 
-import axios from "axios";
-import { useEffect, useState } from "react";
 
-export default function UsersPage() {
-  const [users, setUsers] = useState<
-    { id: number; name: string; email: string; role: string }[]
-  >([]);
-  const [search, setSearch] = useState<string>("");
-  console.log(users);
-  console.log(users);
-  
-
+const UserPage = () => {
+  const [users, setUsers] = useState<User[]>([])
+   const [search, setSearch] = useState<string>("");
   useEffect(() => {
-    const fetchUsers = async () => {
-      try {
-        const res = await axios.get(
-          "http://localhost:8000/api/admin/users",
-
-          {
-            headers: {
-              Authorization: `Bearer ${localStorage.getItem("token")}`,
-            },
+        const fetchUsers = async () => {
+          try {
+            const res = await axios.get(
+              "http://localhost:8000/api/admin/users",
+    
+              {
+                headers: {
+                  Authorization: `Bearer ${localStorage.getItem("token")}`,
+                },
+              }
+            );
+          
+    
+            if (res.status === 200) {
+          
+              setUsers(res.data);
+            } else {
+              alert("Failed to load Users");
+            }
+          } catch (err) {
+            console.error(err);
+            alert("Error fetching Users");
           }
-        );
-      
-
-        if (res.status === 200) {
-      
-          setUsers(res.data);
-        } else {
-          alert("Failed to load Users");
+        };
+        fetchUsers();
+      }, []);
+    
+      const handleDelete = async (id: number) => {
+        try {
+          const res = await axios.delete(
+            `http://localhost:8000/api/admin/users/${id}`,
+            {
+              headers: {
+                Authorization: `Bearer ${localStorage.getItem("token")}`,
+              },
+            }
+          );
+         
+       
+          if (res.status === 200) {
+            setUsers(users.filter((user) => Number(user.id) !== id));
+            alert("User deleted successfully");
+          } else {
+            alert("Failed to delete User");
+          }
+        } catch (err) {
+          console.error(err);
+          alert("Error deleting User");
         }
-      } catch (err) {
-        console.error(err);
-        alert("Error fetching Users");
       }
-    };
-    fetchUsers();
-  }, []);
-
-  const handleDelete = async (id: number) => {
-    try {
-      const res = await axios.delete(
-        `http://localhost:8000/api/admin/users/${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${localStorage.getItem("token")}`,
-          },
-        }
-      );
-      console.log(res);
       
-      if (res.status === 200) {
-        setUsers(users.filter((user) => Number(user.id) !== id));
-        alert("User deleted successfully");
-      } else {
-        alert("Failed to delete User");
-      }
-    } catch (err) {
-      console.error(err);
-      alert("Error deleting User");
-    }
-  }
-  
-
-
+      
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold mb-4">All Users</h1>
+    <div>
+      User Dashboard
+      <div className='mt-10 text-center bold text-2xl'>
+        All Users
+      </div>
       <input
         type="text"
         placeholder="Search by Name"
@@ -76,24 +178,40 @@ export default function UsersPage() {
         onChange={(e) => setSearch(e.target.value)}
         className="border p-2 mb-4 rounded w-full"
       />
-      <ul className="space-y-2">
-        {users.filter((value)=>search.toLowerCase()===""? value: value.name.startsWith(search)).map((user) => (
-          <li key={user.id} className="border p-3 rounded-md">
-            <p>
-              <b>Name:</b> {user.name}
-            </p>
-            <p>
-              <b>Email:</b> {user.email}
-            </p>
-            <p>
-              <b>Role:</b> {user.role}
-            </p>
-            <button className="bg-blue-500 text-white px-3 py-1 rounded" onClick={() => handleDelete(Number(user.id))}>
-              Delete
-            </button>
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
+        <div>
+        <table className='w-full border mt-12'>
+          <thead>
+            <tr className='bg-gray-200 '>
+              <th className='p-2'>ID</th>
+              <th className='p-2'>Name</th>
+              <th className='p-2'>Email</th>
+              <th className='p-2'>Role</th>
+              <th className='p-2'>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.filter((value)=>search.toLowerCase()===""? value: value.name.startsWith(search)).map((user) => (
+              <tr key={user.id} className='border-t text-center'>
+                <td className='p-2'>{user.id}</td>
+                <td className='p-2'>{user.name}</td>
+                <td className='p-2'>{user.email}</td>
+                <td className='p-2'>{user.role}</td>
+                <td className='p-2'>
+                  <button className='bg-blue-500 text-white px-3 py-1 rounded' onClick={() => handleDelete(Number(user.id))}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        </div>
+        
+        
+      </div>
+   
+    
+  )
 }
+
+export default UserPage
