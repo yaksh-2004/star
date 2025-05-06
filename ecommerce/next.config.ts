3c623087295d9317ef93882c57d79b95890b0045
Yaksh <yakshparikh2004@gmail.com>
2025-05-06 12:25:55 +0530

    update

--- ecommerce/next.config.ts
+++ ecommerce/next.config.ts
@@ -8,7 +8,7 @@ const nextConfig: NextConfig = {
         protocol: "http",
         hostname: "localhost",
         port: "8000",
-        pathname: "/**",
+        pathname: "/uploads",
       },
     ],
   }
